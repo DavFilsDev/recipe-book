41b1367a9b674745fb700f5be0a9322b53e52e00
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-27 14:45:06 +0300

    feat(app): add favorites state and toggleFavorite logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,22 @@ import RecipeList from './components/RecipeList/RecipeList.jsx'
 
 export default function App() {
   const [orderedRecipes, setOrderedRecipes] = useState(recipes)
+  const [favorites, setFavorites] = useState([])
 
   function handleToggleOrder() {
     setOrderedRecipes((prev) => [...prev].reverse())
+  }
+
+  function toggleFavorite(recipe) {
+    setFavorites((prev) => {
+      const exists = prev.find(r => r.id === recipe.id)
+
+      if (exists) {
+        return prev.filter(r => r.id !== recipe.id)
+      } else {
+        return [...prev, recipe]
+      }
+    })
   }
 
   return (
@@ -25,7 +38,19 @@ export default function App() {
         </div>
       </header>
       <main className={styles.main}>
-        <RecipeList recipes={orderedRecipes} />
+        <div>
+          <h3>Favorites:</h3>
+          <ul>
+            {favorites.map((r) => (
+              <li key={r.id}>{r.name}</li>
+            ))}
+          </ul>
+        </div>
+        <RecipeList
+          recipes={orderedRecipes}
+          favorites={favorites}
+          toggleFavorite={toggleFavorite}
+        />
       </main>
     </div>
   )
